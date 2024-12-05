61327c7900266af0526bfcb958dbbc90153c1d3d
Ranis Zyalalov <Jabelya@yandex.ru>
2024-12-05 19:47:40 +0300

    ТЗ

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,54 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.validate.Validation;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/films")
+@Slf4j
 public class FilmController {
+
+    Validation validation = new Validation();
+    Map<Long, Film> allFilms = new HashMap<>();
+
+    @GetMapping
+    public Collection<Film> getAllFilms() {
+        log.info("Получаем данные об всех фильмах");
+        return allFilms.values();
+    }
+
+    @PostMapping
+    public Film postFilm(@RequestBody Film postFilm) {
+        log.info("Пошел процесс добавление фильма " + postFilm);
+        postFilm = validation.checkValidationFilm(postFilm);
+        long id = getNextId();
+        postFilm.setId(id);
+        allFilms.put(postFilm.getId(), postFilm);
+        log.info("Фильм добавлен в коллекцию: " + postFilm);
+        return postFilm;
+    }
+
+    @PutMapping
+    public Film putFilm(@RequestBody Film putFilm) {
+        log.info("Пошел процесс обновление фильма " + putFilm);
+        putFilm = validation.checkValidationFilmOnPut(allFilms.keySet(), putFilm);
+        allFilms.put(putFilm.getId(), putFilm);
+        log.info("Фильм обновлен в коллекции: " + putFilm);
+        return putFilm;
+    }
+
+    private long getNextId() {
+        long currentMaxId = allFilms.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
 }
---
+++ src/main/java/ru/yandex/practicum/filmorate/validate/Validation.java
@@ -0,0 +1,143 @@
+package ru.yandex.practicum.filmorate.validate;
+
+import lombok.extern.slf4j.Slf4j;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.util.Set;
+
+@Slf4j
+public class Validation {
+
+    private final int MAX_LENGTH_DESCRIPTION = 200;
+    private final LocalDate MIN_RELEASE_DATA = LocalDate.of(1985, 12, 28);
+    private final char CHECK_SYMBOL = '@';
+
+    public Film checkValidationFilm(Film film) {
+        log.info("Проверка имени");
+        checkNameFilm(film.getName());
+        log.info("Проверка длины");
+        maxLengthDescription(film.getDescription());
+        log.debug("Проверка минимальной даты");
+        minDateReleaseDate(film.getReleaseDate());
+        log.info("Проверка продолжительности");
+        positiveDurationFilm(film.getDuration());
+        return film;
+    }
+
+    public Film checkValidationFilmOnPut(Set<Long> allId, Film film) {
+        for (Long l : allId) {
+            if (film.getId() == l) {
+                checkValidationFilm(film);
+                return film;
+            }
+        }
+        throw new ValidationException("Нет такого id в списке");
+    }
+
+    public User checkValidationUser(User user) {
+        checkEmail(user.getEmail());
+        checkLogin(user.getLogin());
+        String nameUser = checkName(user.getLogin(), user.getName());
+        user.setName(nameUser);
+        checkBirthday(user.getBirthday());
+        return user;
+    }
+
+    public User checkValidationUserOnPut(Set<Long> allId, User user) {
+        for (Long l : allId) {
+            if (user.getId() == l) {
+                checkValidationUser(user);
+                return user;
+            }
+        }
+        throw new ValidationException("Нет такого id в списке");
+    }
+
+    public boolean checkNameFilm(String name) {
+        if (name == null || name.isBlank()) {
+            log.info("Название фильма не может быть пустым");
+            throw new ValidationException("Название фильма не может быть пустым");
+        } else {
+            return true;
+        }
+    }
+
+    public boolean maxLengthDescription(String description) {
+        if (description.length() > MAX_LENGTH_DESCRIPTION) {
+            log.info("Максимальная длина описания — 200 символов");
+            throw new ValidationException("Максимальная длина описания — 200 символов");
+        } else {
+            return true;
+        }
+    }
+
+    public boolean minDateReleaseDate(LocalDate date) {
+        if (date.getYear() < MIN_RELEASE_DATA.getYear()) {
+            log.info("Дата релиза — не раньше 28 декабря 1895 года");
+            throw new ValidationException("Дата релиза — не раньше 28 декабря 1895 года");
+        } else if (date.getYear() == MIN_RELEASE_DATA.getYear()) {
+            if (date.getDayOfYear() < MIN_RELEASE_DATA.getDayOfYear()) {
+                log.info("Дата релиза — не раньше 28 декабря год совпадает");
+                throw new ValidationException("Дата релиза — не раньше 28 декабря 1895 года");
+            } else {
+                return true;
+            }
+        } else {
+            return true;
+        }
+    }
+
+    public boolean positiveDurationFilm(Duration duration) {
+        if (duration.isPositive()) {
+            return true;
+        } else {
+            log.info("Продолжительность фильма должна быть положительным числом");
+            throw new ValidationException("Продолжительность фильма должна быть положительным числом");
+        }
+    }
+
+    public boolean checkEmail(String email) {
+        if (email == null || email.isBlank() || email.indexOf(CHECK_SYMBOL) < 0) {
+            log.info("Электронная почта не может быть пустой и должна содержать символ @");
+            throw new ValidationException("Электронная почта не может быть пустой и должна содержать символ @");
+        }
+        return true;
+    }
+
+    public String checkName(String login, String name) {
+        if (name == null || name.isBlank()) {
+            return login;
+        } else {
+            return name;
+        }
+    }
+
+    public boolean checkBirthday(LocalDate localDate) {
+        LocalDate localDateNow = LocalDate.now();
+        if (localDateNow.getYear() < localDate.getYear()) {
+            log.info("Год не может быть выше текущего года");
+            throw new ValidationException("Дата рождения не может быть в будущем");
+        } else if (localDateNow.getYear() == localDate.getYear()) {
+            log.info("Года совпадают");
+            if (localDateNow.getDayOfYear() < localDate.getDayOfYear()) {
+                throw new ValidationException("Дата рождения не может быть в будущем");
+            }
+            return true;
+        } else {
+            return true;
+        }
+    }
+
+    public String checkLogin(String login) {
+        if (login == null || login.isBlank()) {
+            log.info("Логин не может быть пустым и содержать пробелы");
+            throw new ValidationException("Логин не может быть пустым и содержать пробелы");
+        } else {
+            return login;
+        }
+    }
+}
