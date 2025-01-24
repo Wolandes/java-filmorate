0b56411ddd7824ebbef19fcded362f113a804a68
Alexey <e.al.vl@yandex.ru>
2025-01-24 15:23:21 +0300

    Technical task sprint 12 update

--- src/main/java/ru/yandex/practicum/filmorate/service/genre/GenreServiceImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/genre/GenreServiceImpl.java
@@ -13,7 +13,7 @@ import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-public class GenreServiceDbImpl implements GenreService {
+public class GenreServiceImpl implements GenreService {
     private final GenreStorage genreStorage;
 
     @Override
--- src/main/java/ru/yandex/practicum/filmorate/service/mpaa/MpaaServiceImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/mpaa/MpaaServiceImpl.java
@@ -13,7 +13,7 @@ import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-public class MpaaServiceDbImpl implements MpaaService {
+public class MpaaServiceImpl implements MpaaService {
     private final MpaaStorage mpaaStorage;
 
     @Override
