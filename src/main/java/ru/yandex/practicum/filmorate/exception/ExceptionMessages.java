634eca5143995c8106870e37adde04c84ecb3d4f
Alexey <e.al.vl@yandex.ru>
2025-02-03 23:57:18 +0300

    Group project add-common-films fix check style

--- src/main/java/ru/yandex/practicum/filmorate/exception/ExceptionMessages.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ExceptionMessages.java
@@ -21,7 +21,6 @@ public class ExceptionMessages {
     public static final String INSERT_DIRECTOR_ERROR = "Не удалось добавить режиссера %s";
     public static final String UPDATE_DIRECTOR_ERROR = "Не удалось изменить режиссера id = %d";
     public static final String DELETE_DIRECTOR_ERROR = "Не удалось удалить режиссера с id = %d";
-    public static final String CHECK_DELETE_DIRECTOR_ERROR = "Не удалось удалить режиссера с id = %d, т.к. он есть в фильмах";
     public static final String DIRECTOR_NOT_FOUND_FROM_LIST_ERROR = "Из списка режиссеров с id %s не все элементы найдены";
     public static final String PARAM_VALUE_FILMS_BY_DIRECTOR_ERROR = "Неверно указан параметр сортировки, вы указали %s, допустимые значения: year, likes";
     public static final String PARAM_VALUE_SEARCH_FILMS_ERROR = "Неверно указан параметр поиска, вы указали %s, допустимые значения: director, title";
--- src/main/java/ru/yandex/practicum/filmorate/storage/director/DirectorDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/director/DirectorDbStorage.java
@@ -17,7 +17,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import static java.util.function.Function.identity;
